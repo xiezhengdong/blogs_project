0c9c284f2d526c445319631dcd07316eae29a70a
huangsh <huangsonghui@wtoip.com>
2018-04-28 17:19:19 +0800

    优化分页

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from blog.models import zhuce,artice
+from blog.models import zhuce,artice,artice_dj,artice_ja,artice_js
 
 # Register your models here.
 
 admin.site.site_header = '博客管理后台'
 admin.site.register(zhuce)
 admin.site.register(artice)
+admin.site.register(artice_dj)
+admin.site.register(artice_ja)
+admin.site.register(artice_js)
